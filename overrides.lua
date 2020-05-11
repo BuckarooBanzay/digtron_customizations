9a2fcd5e8fba4713e184d75e612bac98edd4fdcd
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-05-11 17:34:43 +0200

    return old values

--- overrides.lua
+++ overrides.lua
@@ -2,8 +2,9 @@
 local old_execute_dig_cycle = digtron.execute_dig_cycle
 
 digtron.execute_dig_cycle = function(...)
-  -- enable protection cache just for the digging movement
-  digtron_customizations.digging = true
-  old_execute_dig_cycle(...)
-  digtron_customizations.digging = false
+	-- enable protection cache just for the digging movement
+	digtron_customizations.digging = true
+	local res1, res2, res3 = old_execute_dig_cycle(...)
+	digtron_customizations.digging = false
+	return res1, res2, res3
 end
